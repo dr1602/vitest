447cadb4ffc6e42db9c8858b08f9eda32988300f
dr1602 <david.arangelg@gmail.com>
2025-01-24 18:05:12 -0600

    feat/1 adding first major test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import React from 'react'
+import './App.css'
+
 function App() {
+  const [buttonState, setButtonState] = React.useState({
+    buttonColor: 'red',
+    buttonDisabled: false,
+  })
+  const nextColor = buttonState.buttonColor === 'red' ? 'blue' : 'red'
+  const handleChangeCheckbox = (event) => {
+    setButtonState((prevState) => ({
+      ...prevState,
+      buttonDisabled: event.target.checked,
+    }))
+  }
+
+  console.log(buttonState.buttonDisabled)
+
   return (
     <div>
-      <h1>I'm gonna learn React Testing Library</h1>
+      <button
+        className={buttonState.buttonColor}
+        disabled={buttonState.buttonDisabled}
+        onClick={
+          () => setButtonState((prevState) => ({
+            ...prevState,
+            buttonColor: nextColor,
+          }))
+        }
+      >
+        Change to {nextColor}
+      </button>
+      <br />
+      <input
+        type='checkbox'
+        id='disable-button-checkbox'
+        checked={buttonState.buttonColor.buttonDisabled}
+        onChange={handleChangeCheckbox}
+      />
+      <label htmlFor='disable-button-checkbox'>Disable button</label>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { logRoles } from '@testing-library/dom'
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { expect } from "vitest";
 
 test("Empty Test", () => {
 
@@ -9,14 +11,49 @@ test("Empty Test", () => {
 //   throw new Error('fail this test!')
 // });
 
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).not.toBeInTheDocument();
-});
+// test("App contains correct heading", () => {
+//   render(<App />);
+//   const headingElement = screen.getByText(/learn react/i);
+//   expect(headingElement).not.toBeInTheDocument();
+// });
 
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
-});
+// test("App contains correct heading", () => {
+//   render(<App />);
+//   const headingElement = screen.getByText(/learn react/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+test('Button click flow', () => {
+  const { container } = render(<App/>)
+  logRoles(container)
+  
+  const buttonElement = screen.getByRole('button', { name: /blue/i });
+
+  expect(buttonElement).toHaveClass('red')
+
+  fireEvent.click(buttonElement)
+
+  expect(buttonElement).toHaveTextContent(/red/i)
+
+  expect(buttonElement).toHaveClass('blue')
+
+})
+
+test('Checkbox flow', () => {
+  render(<App/>)
+
+  const buttonElement = screen.getByRole('button', { name: /blue/i })
+  const checkboxElement = screen.getByRole('checkbox', { name: /disable button/i })
+
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+
+  fireEvent.click(checkboxElement);
+  expect(checkboxElement).toBeChecked();
+  expect(buttonElement).toBeDisabled();
+
+  fireEvent.click(checkboxElement);
+  expect(checkboxElement).not.toBeChecked();
+  expect(buttonElement).toBeEnabled();
+
+})
